6480fafd8ff5e7511c88650d68cee4b8fb5a3244
mahdi-shokrzadeh <74652955+mahdi-shokrzadeh@users.noreply.github.com>
2024-03-05 15:11:17 +0330

    Updating services.

--- services/WeatherService/weatherService.js
+++ services/WeatherService/weatherService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const WEATHER_API_KEY = "614fc7b8530705bcff92e7a0db8d06a0";
+const WEATHER_API_KEY = "";
 const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
 export const getCurrentWeather =  (lat , lon) => {
